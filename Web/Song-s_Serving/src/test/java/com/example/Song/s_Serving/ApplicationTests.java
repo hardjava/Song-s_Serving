9b9ba6b03efab2617a1566606407d355f28ee217
hardjava <fkdlcn123@kumoh.ac.kr>
2025-04-09 22:49:07 +0900

    temp

---
+++ Web/Song-s_Serving/src/test/java/com/example/Song/s_Serving/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Song.s_Serving;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
